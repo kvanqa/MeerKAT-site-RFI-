8873d0ed15a5ac36a6f4ee0c2a6eb8ff6148d324
isaac <isihlangu@ska.ac.za>
2019-03-04 08:32:04 +0000

    setup script

---
+++ kathprfi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kathprfi',
+      version='0.1',
+      description='MeerKAT historical probability RFI code',
+      url='https://github.com/SihlanguI/kathprfi',
+      author='Isaac Sihlnagu',
+      author_email='isihlangu@ska.ac.za',
+      license='SARAO',
+      packages=['kathprfi'],
+zip_safe=False)
