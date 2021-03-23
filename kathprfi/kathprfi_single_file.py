70a910b2053de1739c41413ad24b7eabe7dab77e
SihlanguI <isihlangu@ska.ac.za>
2021-03-23 14:03:00 +0000

    flake-8 standars and config file

---
+++ kathprfi/kathprfi_single_file.py
@@ -0,0 +1,311 @@
+import datetime
+
+import katdal
+import numpy as np
+import pandas as pd
+from numba import jit
+from numba import prange
+
+
+def readfile(path):
+    """
+    Read in the RDB file.
+
+    Parameters
+    ----------
+    path : str
+        RDB file
+
+    Returns
+    -------
+    output_file : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+    """
+    vis = katdal.open(path)
+    return vis
+
+
+def config2dic(filepath):
+    """
+    Read a configuration file which will be passed into
+    a new object instance.
+
+    Parameters:
+    ----------
+    filepath : string
+        The absolute filepath of the config file.
+  
+    Returns:
+    --------
+    args_dict : dict
+        A dictionary of arguments, to be passed into some function,
+        usually the katdal object instance.
+   """
+
+    #open file and read contents
+    config_file = open(filepath)
+    txt = config_file.read()
+    args_dict = {}
+
+    #set up dictionary of arguments based on their types
+    for line in txt.split('\n'):
+        if len(line) > 0 and line.replace(' ','')[0] != '#':
+            #use '=' as delimiter and strip whitespace
+            split = line.split('=')
+            key = split[0].strip()
+            val = split[1].strip()
+            args_dict.update({key : val})
+    config_file.close()
+    return args_dict
+
+
+def remove_bad_ants(vis):
+    """
+    Extract good antennas by checking antenna activity list.
+
+    Parameters
+    ----------
+    vis : katdal.visdatav4.VisibilityDataV4
+        katdal data object
+
+    Returns
+    ---------
+    output : python list
+          List of good antennas
+    """
+    # This pull all the antenna used for observation
+    AntList = []
+    for ant in vis.ants:
+        AntList.append(ant.name)
+
+    # This will give the antenna activity list
+    AntsActivity = []
+    for AntName in AntList:
+        AntsActivity.append((AntName, vis.sensor[AntName+'_activity']))
+    # Check if there is a stop state in antenna activity list and if true remove
+    # the antenna
+    for i in range(len(AntsActivity)):
+        if 'stop' in AntsActivity[i][1]:
+            AntList.remove(AntsActivity[i][0])
+        else:
+            pass
+    return AntList
+
+
+def selection(vis, pol_to_use, corrprod, scan, clean_ants, flag_type):
+    """
+    Do subselection of the dataset based on the given parameters.
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+    pol_to_use : str
+        polarization product to use [HH or VV or VH or HV] default: 'HH'
+    corrprod : str
+        type of correlation product, either 'cross' or 'auto'. default: 'cross'
+    scan : str
+        type of a scan to use [track or slew]. default: 'track'
+    clean_ants : python list
+        list of clean antennas
+    flag_type : python list
+        type of flag/s [cal_rfi, ingest_rfi, cam]. default: cal_rfi
+
+    Returns:
+    --------
+    output : katdal.lazy_indexer.DaskLazyIndexer
+        sub selected katdal lazy indexer of RFI flags
+    """
+    vis.select(corrprods=corrprod, pol=pol_to_use, scans=scan, ants=clean_ants,
+               flags=flag_type)
+    flag = vis.flags
+    return flag
+
+
+def get_az_and_el(vis):
+    """
+    Get the telescope pointings (i.e. elevation and azimuth).
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+
+    Returns:
+    --------
+    output: numpy arrays
+        numpy arrays of avaraged elevation and azimuth of all antennas per each time stamp
+    """
+    # Getting the azmuth and elevation
+    azmean = np.mean(vis.az, axis=1) % 360
+    elmean = np.mean(vis.el, axis=1)
+    return elmean, azmean
+
+
+def get_time_idx(vis):
+    """
+    Convert unix time to hour of a day.
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+
+    Returns:
+    --------
+    output: numpy array
+       numpy array with time dumps converted to hour of a day
+    """
+    unix = vis.timestamps
+    local_time = []
+    for i in range(len(unix)):
+        local_time.append(datetime.datetime.fromtimestamp((unix[i])).strftime('%H:%M:%S'))
+    # Converting time to hour of a day
+    hour = []
+    for i in range(len(local_time)):
+        h = int(round(int(local_time[i][:2]) + int(local_time[i][3:5])/60 + float(
+            local_time[i][-2:])/3600))
+        if h == 24:
+            hour.append(0)
+        else:
+            hour.append(h)
+    return np.array(hour, dtype=np.int32)
+
+
+def get_az_idx(azimuth, azbins):
+    """
+    Get the azimuth angle indices.
+
+    Parameters:
+    -----------
+    azimuth : numpy array
+           array of Azimuthal angle
+    azbins : numpy array
+         array of azimuthal bins
+
+    Returns:
+    --------
+    output : numpy array
+     array of elevation indices
+    """
+    az_idx = []
+    for az in azimuth:
+        for j in range(len(azbins)-1):
+            if azbins[j] <= az < azbins[j+1]:
+                az_idx.append(j)
+    return np.array(az_idx)
+
+
+def get_el_idx(elevation, elbins):
+    """
+    Get the elevation angle indices.
+
+    Parameters:
+    -----------
+    elevation : numpy array
+        array of elevation angles
+    azbins : numpy array
+        array of elevation bins
+
+    output : numpy array
+       array of elevation indices
+    """
+    el_idx = []
+
+    for el in elevation:
+        for j in range(len(elbins)):
+            if elbins[j] <= el < elbins[j]+10:
+                el_idx.append(j)
+    return np.array(el_idx, dtype=np.int32)
+
+
+def get_corrprods(vis):
+    """
+    Get the correlation products
+
+    Parameters:
+    ----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+
+    Returns:
+    --------
+    output : numpy array
+         array of correlation products
+    """
+    bl = vis.corr_products
+    bl_idx = []
+    for i in range(len(bl)):
+        bl_idx.append((bl[i][0][0:-1]+bl[i][1][0:-1]))
+    return np.array(bl_idx)
+
+
+def get_bl_idx(vis, nant):
+    """
+    Get the indices of the correlation products.
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+    nant : int
+       number of antennas
+
+    Returns:
+    --------
+    output : numpy array
+       array of baseline indices
+    """
+    nant = nant
+    A1, A2 = np.triu_indices(nant, 1)
+    # Creating baseline antenna combinations
+    corr_products = np.array(['m{:03d}m{:03d}'.format(A1[i], A2[i]) for i in range(len(A1))])
+    df = pd.DataFrame(data=np.arange(len(A1)), index=corr_products).T
+    corr_prods = get_corrprods(vis)
+    bl_idx = df[corr_prods].values[0].astype(np.int32)
+    return bl_idx
+
+
+@jit(nopython=True, parallel=True)
+def update_arrays(vis, time_slice, azbins, elbins, nant, Good_flags, Master, Counter):
+    """
+    Update the master and counter array
+
+    Parameters:
+    -----------
+    vis : katdal.visdatav4.VisibilityDataV4
+       katdal data object
+    azbins : numpy array
+        array of azimuthal bins
+    elbins : numpy array
+        array of elevation bins
+    nant : int
+       number of antennas
+    Good_flags : katdal.lazy_indexer.DaskLazyIndexer
+        sub selected katdal lazy indexer of RFI flags
+    Master : numpy array
+       array contains the number of RFI points per voxel with dimension of [T, F, B, Az, El]
+    Counter : numpy array
+      array contains the total number of observations per voxel with dimension of [T, F, B, Az, El]
+
+    Returns:
+    -------
+    output : numpy array
+      updated master and counter array
+    """
+    Time_idx = get_time_idx(vis)[time_slice]
+    Bl_idx = get_bl_idx(vis, nant)
+    el, az = get_az_and_el(vis)
+    El_idx = get_el_idx(el, elbins)[time_slice]
+    Az_idx = get_az_idx(az, azbins)[time_slice]
+    cstep = 128
+    cblocks = (4096 + cstep - 1) // cstep
+    for cblock in prange(cblocks):
+        c_start = cblock * cstep
+        c_end = min(4096, c_start + cstep)
+        for k in range(c_start, c_end):
+            for i in range(len(Bl_idx)):
+                for j in range(len(Time_idx)):
+                    Master[Time_idx[j], k, Bl_idx[i], El_idx[j], Az_idx[j]] += Good_flags[j, k, i]
+                    Counter[Time_idx[j], k, Bl_idx[i], El_idx[j], Az_idx[j]] += 1
+    return Master, Counter
---
+++ script/kathprfi_script.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+import argparse
+import ast
+import logging
+import os
+import time as tme
+
+import numpy as np
+import pandas as pd
+
+import kathprfi_single_file as kathp
+
+
+def initialize_logs():
+    """
+    Initialize the log settings
+    """
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='This package produces two 5-D arrays, '
+                                                 'which are the counter array and the master array.'
+                                                 'The arrays provides statistics about measured'
+                                                 'RFI from MeerKAT telescope.')
+    parser.add_argument('-c', '--config', action='store', type=str,
+                       help='A config file that does subselction of data')
+    parser.add_argument('-b', '--bad', action='store',  type=str,
+                        help='Path to save list of bad files')
+    parser.add_argument('-g', '--good', action='store', type=str, default='\tmp',
+                        help='Path to save bad files')
+    parser.add_argument('-z', '--zarr', action='store', type=str, default='\tmp',
+                        help='path to save output zarr file')
+    return parser
+
+
+def main():
+    # Initializing the log settings
+    initialize_logs()
+    logging.info('MEERKAT HISTORICAL PROBABILITY OF RADIO FREQUENCY INTERFERENCE FRAMEWORK.')
+    parser = create_parser()
+    args = parser.parse_args()
+    path2config = os.path.abspath(args.config)
+    # Read in dictionary with keys and values from config file
+    config = kathp.config2dic(path2config)
+    # Get values from the dictionary
+    filename = config['filename']
+    name_col = config['name_col']
+    corrpro = config['corrprod']
+    scans = config['scan']
+    flags = ['flag_type']
+    pol = ['pol_to_use']
+    pol = ast.literal_eval(pol)
+    dump_rate = int(config['dump_period'])
+    correlator_mode = config['correlator_mode']
+    if correlator_mode == '4k':
+        freq_chan = 4096
+    elif correlator_mode == '32k':
+        freq_chan = 32000
+    # Read in csv file with files to process
+    data = pd.read_csv(filename)
+    f = data[name_col].values
+    badfiles = []
+    goodfiles = []
+    for i in range(len(f)):
+        # Initializing 5-D arrays
+        master = np.zeros((24, freq_chan, 2016, 8, 24), dtype=np.uint16)
+        counter = np.zeros((24, freq_chan, 2016, 8, 24), dtype=np.uint16)
+        s = tme.time()
+        logging.info('Adding file {} : {}'.format(i, f[i]))
+        try:
+            pathvis = f[i]
+            vis = kathp.readfile(pathvis)
+            logging.info('File number {} has been read'.format(i))
+            if len(vis.freqs) == freq_chan and vis.dump_period > dump_rate-1 and vis.dump_period <= dump_rate:
+                logging.info('Removing bad antennas')
+                clean_ants = kathp.remove_bad_ants(vis)
+                logging.info('Bad antennas has been removed.')
+                good_flags = kathp.selection(vis, pol_to_use=pol, corrprod=corrpro, scan=scans,
+                                             clean_ants=clean_ants, flag_type=flags)
+                logging.info('Good flags has been returned')
+                if good_flags.shape[0] * good_flags.shape[1] * good_flags.shape[2] != 0:
+                    # create azimuth and elevation bins
+                    azbins = get_az_idx(az, np.arange(0, 370, 15))
+                    elbins = get_el_idx(el, np.arange(10, 90, 10))
+                    # Updating the array
+                    ntime = good_flags.shape[0]
+                    time_step = 10
+                    if ntime <= time_step:
+                        time_step = ntime
+                    for tm in six.moves.range(0, ntime, time_step):
+                        time_slice = slice(tm, tm + time_step)
+                        flag_chunk = good_flags[time_slice].astype(int)
+                        master, counter = kathp.update_arrays(vis, time_slice, azbins, elbins, nant,
+                                                              flag_chunk, Master, Counter)
+                    logging.info('{} s has been taken to update file number {}'.format(i,
+                                                                                       tme.time()
+                                                                                       - s))
+                    goodfiles.append(f[i])
+                    logging.info('Creating Xarray Dataset')
+                    ds = xr.Dataset({'master': (('time', 'frequency', 'baseline', 'elevation',
+                                                 'azimuth'), master),
+                   'counter': (('time', 'frequency', 'baseline', 'elevation', 'azimuth'), counter)},
+                   {'time': np.arange(24), 'frequency': vis.freqs, 'baseline': np.arange(2016),
+                   'elevation': np.linspace(10, 80, 8), 'azimuth': np.arange(0, 360, 15)})
+                    logging.info('Saving dataset')
+                    ds.to_zarr(args.zarr+str(f[i]),'w')
+                    logging.info('Dataset has been saved')
+                else:
+                    logging.info('{} selection has a problem'.format(f[i]))
+                    badfiles.append(f[i])
+                    pass
+            else:
+                logging.info('Channel/dump has a problem')
+                badfiles.append(f[i])
+                pass
+            np.save(args.good,goodfiles)
+            np.save(args.bad,badfiles)
+            logging.info('File has been saved')
+
+        except Exception as e:
+            logging.info(e)
+            continue
+
+
+if __name__=="__main__":
+    main()
+                   
